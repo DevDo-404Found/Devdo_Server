98079886dec7d1e3184115ba2a8e4dc360827080
ttttkii913 <dbgkdud821@daum.net>
2025-07-11 20:06:40 +0900

    Feat/#4: Kakao Login 추가

--- DevDo/src/main/java/com/devdo/global/oauth2/AuthLoginController.java
+++ DevDo/src/main/java/com/devdo/global/oauth2/AuthLoginController.java
@@ -2,6 +2,8 @@ package com.devdo.global.oauth2;
 
 import com.devdo.global.oauth2.google.api.dto.GoogleToken;
 import com.devdo.global.oauth2.google.application.GoogleLoginService;
+import com.devdo.global.oauth2.kakao.api.dto.KakaoToken;
+import com.devdo.global.oauth2.kakao.application.KakaoLoginService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthLoginController {
 
     private final GoogleLoginService googleLoginService;
+    private final KakaoLoginService kakaoLoginService;
 
     @Operation(summary = "구글 로그인", description = "구글 로그인 콜백 api입니다.")
     @ApiResponses(value = {
@@ -31,6 +34,21 @@ public class AuthLoginController {
 
     public GoogleToken loginOrSignup(String googleAccessToken){
         return googleLoginService.loginOrSignUp(googleAccessToken);
+    }
+
+    @Operation(summary = "카카오 인가 코드 발급 후 액세스 토큰 리다이렉트", description = "카카오 인가 코드 발급 후 액세스 토큰을 리다이렉트 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "응답 생성에 성공하였습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다.")
+    })
+    @GetMapping("/kakao")
+    public KakaoToken kakaoCallback(@RequestParam(name = "code") String code) {
+        String kakaoAccessToken = kakaoLoginService.getKakaoAccessToken(code);
+        return signUpOrSignInWithKakao(kakaoAccessToken);
+    }
+
+    public KakaoToken signUpOrSignInWithKakao(String kakaoAccessToken) {
+        return kakaoLoginService.signUpOrSignInWithKakao(kakaoAccessToken);
     }
 
 }
