a2453d7995db722da9ac3da5d637e2e401c5f340
ttttkii913 <dbgkdud821@daum.net>
2025-07-11 18:10:19 +0900

    Feat/#4: Member domain 작성

---
+++ DevDo/src/main/java/com/devdo/member/domain/Member.java
@@ -0,0 +1,38 @@
+package com.devdo.member.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "member_nickname")
+    private String nickname;
+
+    @Column(name = "member_email", nullable = false)
+    private String email;
+
+    @Column(name = "member_picture_url")
+    private String pictureUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "social_type", nullable = false)
+    private SocialType socialType;
+
+    @Builder
+    public Member(String nickname, String email, String pictureUrl, SocialType socialType) {
+        this.nickname = nickname;
+        this.email = email;
+        this.pictureUrl = pictureUrl;
+        this.socialType = socialType;
+    }
+}
