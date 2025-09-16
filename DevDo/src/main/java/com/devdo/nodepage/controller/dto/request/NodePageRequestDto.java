278f1626ecc39113000c47b4b5eb66ba65dbb592
Lee, yeeun <ye7426@naver.com>
2025-09-16 09:51:06 +0900

    Feat/#56: S3 이미지 업로드 기능 구현

--- DevDo/src/main/java/com/devdo/nodepage/controller/dto/request/NodePageRequestDto.java
+++ DevDo/src/main/java/com/devdo/nodepage/controller/dto/request/NodePageRequestDto.java
@@ -1,8 +1,11 @@
 package com.devdo.nodepage.controller.dto.request;
 
+import io.micrometer.common.lang.Nullable;
+import org.springframework.web.multipart.MultipartFile;
+
 public record NodePageRequestDto(
         String content,
         String emoji,
-        String pictureUrl
+        MultipartFile pictureFile
 ) {
 }
