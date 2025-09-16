231a9c82d528aadec1edd8e45442a4e39dd39e4e
Lee, yeeun <ye7426@naver.com>
2025-09-16 10:45:51 +0900

    Feat/#65: JSON 파싱 이슈 해결

--- DevDo/src/main/java/com/devdo/nodepage/controller/dto/request/NodePageRequestDto.java
+++ DevDo/src/main/java/com/devdo/nodepage/controller/dto/request/NodePageRequestDto.java
@@ -1,6 +1,10 @@
 package com.devdo.nodepage.controller.dto.request;
 
+import com.devdo.common.jackson.NewlineSanitizer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 public record NodePageRequestDto(
+        @JsonDeserialize(using = NewlineSanitizer.class)
         String content,
         String emoji,
         String pictureUrl
