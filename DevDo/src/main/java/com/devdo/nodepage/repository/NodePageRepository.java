35981b52dd305ab19be52b85b022c517b3b8ad8b
Lee, yeeun <ye7426@naver.com>
2025-09-20 00:02:38 +0900

    Feat/#69: S3 스케줄러를 이용한 미사용 이미지 자동 삭제

--- DevDo/src/main/java/com/devdo/nodepage/repository/NodePageRepository.java
+++ DevDo/src/main/java/com/devdo/nodepage/repository/NodePageRepository.java
@@ -3,8 +3,13 @@ package com.devdo.nodepage.repository;
 import com.devdo.nodepage.entity.NodePage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface NodePageRepository extends JpaRepository<NodePage, Long> {
     Optional<NodePage> findByNode_NodeId(Long nodeId);
+
+    // Node와 연결되지 않고 사진이 있는 NodePage를 찾는 새로운 쿼리 메서드
+    List<NodePage> findByPictureUrlIsNotNullAndNodeIsNullAndCreatedAtBefore(LocalDateTime timestamp);
 }
