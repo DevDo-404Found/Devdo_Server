44a08f84315fa906e417279c37d37e608858c632
ttttkii913 <dbgkdud821@daum.net>
2025-09-19 16:08:45 +0900

    Refactor/#59: Follow 요청 응답 추가

--- DevDo/src/main/java/com/devdo/follow/api/dto/response/FollowResDto.java
+++ DevDo/src/main/java/com/devdo/follow/api/dto/response/FollowResDto.java
@@ -1,19 +1,34 @@
 package com.devdo.follow.api.dto.response;
 
 import com.devdo.member.api.dto.response.MemberInfoResDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record FollowResDto(
     int followerCount,
     int followingCount,
-    List<MemberInfoResDto> members
+    List<MemberInfoResDto> members,
+    Boolean isFollowing
 ) {
+    // 리스트 조회시 응답
     public static FollowResDto from(int followerCount, int followingCount, List<MemberInfoResDto> members) {
         return new FollowResDto(
                 followerCount,
                 followingCount,
-                members
+                members,
+                null
+        );
+    }
+
+    // post, delete 요청 시 t/f 응답
+    public static FollowResDto from(int followerCount, int followingCount, Boolean isFollowing) {
+        return new FollowResDto(
+                followerCount,
+                followingCount,
+                null,
+                isFollowing
         );
     }
 }
